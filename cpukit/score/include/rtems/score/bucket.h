c3506480297aa76bfe8e70604d99019fbc16bc59
Timo Gojowczyk <gojowczyk@ls12pc6.cs.uni-dortmund.de>
2018-04-05 13:37:54 +0200

    add bucket.h bucketimpls.h

---
+++ cpukit/score/include/rtems/score/bucket.h
@@ -0,0 +1,23 @@
+/* This file definies the structure of a bucket, it contains (currently) an array with the Elements 
+ * and the number of the amount of elements that are in the array. 
+ * and the insert,remove and get_first function */ 
+
+
+
+
+  #ifndef _RTEMS_SCORE_BUCKET_H
+  #define _RTEMS_SCORE_BUCKET_H
+
+  #include <rtems/score/watchdog.h>
+  #include <rtems/score/watchdogimplh.>
+
+  typedef struct
+  {
+    int amount_elements;
+    Watchdog_Control first;
+    Watchdog_Control last;
+  }Bucket;
+
+
+  #endif
+  /* end of include file */
